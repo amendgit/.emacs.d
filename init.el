c7ed34ce46a6201f8a882c53f877b20813827d6a
辰申 <chenshen.sj@alibaba-inc.com>
2015-07-01 11:19:08 +0800

    accelerate the load for emacs.

--- init.el
+++ init.el
@@ -39,58 +39,96 @@
 (require-package 'diminish)
 (require-package 'scratch)
 (require-package 'mwe-log-commands)
+(require-package 'idle-require)
 
 (require 'init-frame-hooks)
 (require 'init-xterm)
 (require 'init-themes)
 (require 'init-osx-keys)
 (require 'init-gui-frames)
-(require 'init-proxies)
-(require 'init-dired)
-(require 'init-isearch)
-(require 'init-grep)
-(require 'init-uniquify)
-(require 'init-ibuffer)
-(require 'init-flycheck)
+; (require 'init-proxies)
+; (require 'init-dired)
+; (require 'init-isearch)
+; (require 'init-grep)
+; (require 'init-uniquify)
+; (require 'init-ibuffer)
+; (require 'init-flycheck)
 
-(require 'init-recentf)
-(require 'init-ido)
-(require 'init-hippie-expand)
-(require 'init-auto-complete)
-(require 'init-windows)
+; (require 'init-recentf)
+; (require 'init-ido)
+; (require 'init-hippie-expand)
+; (require 'init-auto-complete)
+; (require 'init-windows)
 (require 'init-sessions)
-(require 'init-fonts)
-(require 'init-mmm)
+; (require 'init-fonts)
+; (require 'init-mmm)
 
 (require 'init-editing-utils)
 
-(require 'init-vc)
-(require 'init-darcs)
-(require 'init-git)
-(require 'init-github)
+; (require 'init-vc)
+; (require 'init-darcs)
+; (require 'init-git)
+; (require 'init-github)
 
-(require 'init-compile)
-(require 'init-crontab)
-(require 'init-textile)
-(require 'init-markdown)
-(require 'init-csv)
-(require 'init-erlang)
-(require 'init-javascript)
-(require 'init-php)
-(require 'init-org)
-(require 'init-nxml)
-(require 'init-html)
-(require 'init-css)
-(require 'init-haml)
-(require 'init-python-mode)
-(require 'init-haskell)
-(require 'init-ruby-mode)
-(require 'init-rails)
-(require 'init-sql)
+; (require 'init-compile)
+; (require 'init-crontab)
+; (require 'init-textile)
+; (require 'init-markdown)
+; (require 'init-csv)
+; (require 'init-erlang)
+; (require 'init-javascript)
+; (require 'init-php)
+; (require 'init-org)
+; (require 'init-nxml)
+; (require 'init-html)
+; (require 'init-css)
+; (require 'init-haml)
+; (require 'init-python-mode)
+; (require 'init-haskell)
+; (require 'init-ruby-mode)
+; (require 'init-rails)
+; (require 'init-sql)
 
-(require 'init-paredit)
-(require 'init-lisp)
-(require 'init-slime)
+; (require 'init-paredit)
+; (require 'init-lisp)
+; (require 'init-slime)
+
+(require 'idle-require)
+;; {{ idle require other stuff
+(setq idle-require-idle-delay 2)
+(setq idle-require-symbols '(init-org
+                             init-markdown
+                             init-recentf
+                             init-ido
+                             init-hippie-expand
+                             init-auto-complete
+                             init-windows
+                             ; init-sessions
+                             init-mmm
+                             init-fonts
+                             init-vc
+                             init-darcs
+                             init-git
+                             init-github
+                             ; init-editing-utils
+                             init-compile
+                             init-crontab
+                             init-textile
+                             init-python-mode
+                             init-lisp
+                             init-slime
+                             init-paredit
+                             init-proxies
+                             init-dired
+                             init-isearch
+                             init-grep
+                             init-ibuffer
+                             init-flycheck
+                             init-uniquify
+                             ))
+(idle-require-mode 1) ;; starts loading
+;; }}
+
 (when (>= emacs-major-version 24)
   (require 'init-clojure)
   (require 'init-clojure-cider))
