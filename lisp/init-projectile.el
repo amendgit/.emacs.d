59b223ab3fa1325cda997c83996d9e9b0f9f7c54
辰申 <chenshen.sj@alibaba-inc.com>
2015-07-02 10:25:50 +0800

    使用Neotree代替speedbar

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -1,21 +1,29 @@
 (require-package 'projectile)
-(require-package 'sr-speedbar)
-(require-package 'projectile-speedbar)
+(require-package 'neotree)
 
 (require 'projectile)
-(require 'sr-speedbar)
-(require 'projectile-speedbar)
+(require 'neotree)
 
-(setq speedbar-use-images nil)
+(when neo-persist-show
+  (add-hook 'popwin:before-popup-hook
+    (lambda () (setq neo-persist-show nil)))
+  (add-hook 'popwin:after-popup-hook
+    (lambda () (setq neo-persist-show t))))
 
-; (with-current-buffer sr-speedbar-buffer-name
-;   (setq window-size-fixed 'width))
+(setq projectile-switch-project-action 'neotree-projectile-action)
 
-(projectile-global-mode)
-(setq projectile-enable-caching t)
-(setq projectile-file-exists-remote-cache-expire nil)
-(setq projectile-file-exists-local-cache-expire (* 5 60))
-
-(global-set-key (kbd "C-x b") 'projectile-speedbar-open-current-buffer-in-tree)
+(defun neotree-project-dir ()
+  "Open NeoTree using the git root."
+  (interactive)
+  (let ((project-dir (projectile-project-root))
+      (file-name (buffer-file-name)))
+    (if project-dir
+        (progn
+          (neotree-dir project-dir)
+          (neotree-find file-name))
+      (message "Could not find git project root."))))
+  
+(global-set-key (kbd "C-x p") 'neotree-project-dir)
+; (global-set-key (kbd "C-x b") 'neotree-project-dir)
 
 (provide 'init-projectile)
