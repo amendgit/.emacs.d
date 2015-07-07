e119fbbce228a53f8531bc4c196ac70292eead18
辰申 <chenshen.sj@alibaba-inc.com>
2015-07-07 20:12:26 +0800

    limit helm promt to 500.

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -40,7 +40,8 @@
            helm-move-to-line-cycle-in-source nil ; move to end or beginning of source when reaching top or bottom of source.
            helm-ff-search-library-in-sexp t ; search for library in `require' and `declare-function' sexp.
            helm-scroll-amount 8 ; scroll 8 lines other window using M-<next>/M-<prior>
-           helm-ff-file-name-history-use-recentf t)
+           helm-ff-file-name-history-use-recentf t
+           helm-candidate-number-limit 500) ; helm recommand it to be 100, but my mac is very fast.
 
      (when (executable-find "curl")
        (setq helm-google-suggest-use-curl-p t))
