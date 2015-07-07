e77a11219bb23e1fea468fdbbab2caa7af1978bd
辰申 <chenshen.sj@alibaba-inc.com>
2015-07-07 23:15:06 +0800

    setup org mode and support Chinese when export to pdf.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -339,5 +339,173 @@ typical word processor."
      (sql . nil)
      (sqlite . t))))
 
+;;------------------------------------------------------------------------------
+;; Copy from another confg.
+;;------------------------------------------------------------------------------
+
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+
+;; NO spell check for embedded snippets
+;; Please note flyspell only use ispell-word
+(defadvice org-mode-flyspell-verify (after org-mode-flyspell-verify-hack activate)
+  (let ((rlt ad-return-value)
+        (begin-regexp "^[ \t]*#\\+begin_\\(src\\|html\\|latex\\)")
+        (end-regexp "^[ \t]*#\\+end_\\(src\\|html\\|latex\\)")
+        old-flag
+        b e)
+    (when ad-return-value
+      (save-excursion
+        (setq old-flag case-fold-search)
+        (setq case-fold-search t)
+        (setq b (re-search-backward begin-regexp nil t))
+        (if b (setq e (re-search-forward end-regexp nil t)))
+        (setq case-fold-search old-flag))
+      (if (and b e (< (point) e)) (setq rlt nil)))
+    (setq ad-return-value rlt)))
+
+;; Org v8 change log:
+;; @see http://orgmode.org/worg/org-8.0.html
+
+;; {{ export org-mode in Chinese into PDF
+;; @see http://freizl.github.io/posts/tech/2012-04-06-export-orgmode-file-in-Chinese.html
+;; and you need install texlive-xetex on different platforms
+;; To install texlive-xetex:
+;;    `sudo USE="cjk" emerge texlive-xetex` on Gentoo Linux
+(setq org-latex-to-pdf-process ;; org v7
+      '("xelatex -interaction nonstopmode -output-directory %o %f"
+        "xelatex -interaction nonstopmode -output-directory %o %f"
+        "xelatex -interaction nonstopmode -output-directory %o %f"))
+(setq org-latex-pdf-process org-latex-to-pdf-process) ;; org v8
+;; }}
+
+(defun my-setup-odt-org-convert-process ()
+  (interactive)
+  (let ((cmd "/Applications/LibreOffice.app/Contents/MacOS/soffice"))
+    (when (and *is-a-mac* (file-exists-p cmd))
+      ;; org v7
+      (setq org-export-odt-convert-processes '(("LibreOffice" "/Applications/LibreOffice.app/Contents/MacOS/soffice --headless --convert-to %f%x --outdir %d %i")))
+      ;; org v8
+      (setq org-odt-convert-processes '(("LibreOffice" "/Applications/LibreOffice.app/Contents/MacOS/soffice --headless --convert-to %f%x --outdir %d %i"))))
+    ))
+
+(my-setup-odt-org-convert-process)
+
+;; @see https://gist.github.com/mwfogleman/95cc60c87a9323876c6c
+(defun narrow-or-widen-dwim ()
+  "If the buffer is narrowed, it widens. Otherwise, it narrows to region, or Org subtree."
+  (interactive)
+  (cond ((buffer-narrowed-p) (widen))
+        ((region-active-p) (narrow-to-region (region-beginning) (region-end)))
+        ((equal major-mode 'org-mode) (org-narrow-to-subtree))
+        (t (error "Please select a region to narrow to"))))
+
+;; Various preferences
+(setq org-log-done t
+      org-completion-use-ido t
+      org-edit-src-content-indentation 0
+      org-edit-timestamp-down-means-later t
+      org-agenda-start-on-weekday nil
+      org-agenda-span 14
+      org-agenda-include-diary t
+      org-agenda-window-setup 'current-window
+      org-fast-tag-selection-single-key 'expert
+      org-export-kill-product-buffer-when-displayed t
+      ;; org v7
+      org-export-odt-preferred-output-format "doc"
+      ;; org v8
+      org-odt-preferred-output-format "doc"
+      org-tags-column 80
+      ;; org-startup-indented t
+      ;; {{ org 8.2.6 has some performance issue. Here is the workaround.
+      ;; @see http://punchagan.muse-amuse.in/posts/how-i-learnt-to-use-emacs-profiler.html
+      org-agenda-inhibit-startup t ;; ~50x speedup
+      org-agenda-use-tag-inheritance nil ;; 3-4x speedup
+      ;; }}
+      )
+
+;; Refile targets include this file and any file contributing to the agenda - up to 5 levels deep
+(setq org-refile-targets (quote ((nil :maxlevel . 5) (org-agenda-files :maxlevel . 5))))
+;; Targets start with the file name - allows creating level 1 tasks
+(setq org-refile-use-outline-path (quote file))
+;; Targets complete in steps so we start with filename, TAB shows the next level of targets etc
+(setq org-outline-path-complete-in-steps t)
+
+
+(setq org-todo-keywords
+      (quote ((sequence "TODO(t)" "STARTED(s)" "|" "DONE(d!/!)")
+              (sequence "WAITING(w@/!)" "SOMEDAY(S)" "PROJECT(P@)" "|" "CANCELLED(c@/!)"))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Org clock
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Change task state to STARTED when clocking in
+(setq org-clock-in-switch-to-state "STARTED")
+;; Save clock data and notes in the LOGBOOK drawer
+(setq org-clock-into-drawer t)
+;; Removes clocked tasks with 0:00 duration
+(setq org-clock-out-remove-zero-time-clocks t)
+
+;; Show the clocked-in task - if any - in the header line
+(defun sanityinc/show-org-clock-in-header-line ()
+  (setq-default header-line-format '((" " org-mode-line-string " "))))
+
+(defun sanityinc/hide-org-clock-from-header-line ()
+  (setq-default header-line-format nil))
+
+(add-hook 'org-clock-in-hook 'sanityinc/show-org-clock-in-header-line)
+(add-hook 'org-clock-out-hook 'sanityinc/hide-org-clock-from-header-line)
+(add-hook 'org-clock-cancel-hook 'sanityinc/hide-org-clock-from-header-line)
+
+(eval-after-load 'org-clock
+  '(progn
+     (define-key org-clock-mode-line-map [header-line mouse-2] 'org-clock-goto)
+     (define-key org-clock-mode-line-map [header-line mouse-1] 'org-clock-menu)))
+
+(eval-after-load 'org
+  '(progn
+     (require 'org-clock)
+                                        ; @see http://irreal.org/blog/?p=671
+     (setq org-src-fontify-natively t)
+     ;; (require 'org-fstree)
+     (defun soft-wrap-lines ()
+       "Make lines wrap at window edge and on word boundary,
+        in current buffer."
+       (interactive)
+       ;; display wrapped lines instead of truncated lines
+       (setq truncate-lines nil)
+       (setq word-wrap t))
+     (add-hook 'org-mode-hook '(lambda ()
+                                 (setq evil-auto-indent nil)
+                                 (soft-wrap-lines)
+                                 ))))
+
+(defadvice org-open-at-point (around org-open-at-point-choose-browser activate)
+  (let ((browse-url-browser-function
+         (cond ((equal (ad-get-arg 0) '(4))
+                'browse-url-generic)
+               ((equal (ad-get-arg 0) '(16))
+                'choose-browser)
+               (t
+                (lambda (url &optional new)
+                  (w3m-browse-url url t))))))
+    ad-do-it))
+
+(defadvice org-publish (around org-publish-advice activate)
+  "Stop running major-mode hook when org-publish"
+  (let ((old load-user-customized-major-mode-hook))
+    (setq load-user-customized-major-mode-hook nil)
+    ad-do-it
+    (setq load-user-customized-major-mode-hook old)))
+
+;; {{ org2nikola set up
+(setq org2nikola-output-root-directory "~/.config/nikola")
+(setq org2nikola-use-google-code-prettify t)
+(setq org2nikola-prettify-unsupported-language
+      '(elisp "lisp"
+              emacs-lisp "lisp"))
+;; }}
 
 (provide 'init-org)
